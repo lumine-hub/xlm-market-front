aa302fed29d191fc79a33892f18eeaf45baa2f1b
lumine-hub <2761151386@qq.com>
2024-07-25 14:46:36 +0800

    feat：2.mock模拟接口数据

--- src/app/pages/lucky/lucky-wheel-page.tsx
+++ src/app/pages/lucky/lucky-wheel-page.tsx
@@ -1,23 +1,52 @@
 "use client"
 
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 // @ts-ignore
 import {LuckyWheel} from '@lucky-canvas/react'
-
+// @ts-ignore
+import {RaffleAwardVO} from "@/types/RaffleAwardVO";
+import {queryRaffleAwardList, randomRaffle} from "@/apis";
 
 export function LuckyWheelPage()  {
     const [blocks] = useState([
         {padding: '10px', background: '#869cfa', imgs: [{src: "/ikun.jpg"}]}
     ])
+    const queryParams = new URLSearchParams(window.location.search);
+    const strategyId = Number(queryParams.get('strategyId'));
 
-    const [prizes] = useState([
-        {background: '#e9e8fe', fonts: [{text: '0'}]},
-        {background: '#b8c5f2', fonts: [{text: '1'}]},
-        {background: '#e9e8fe', fonts: [{text: '2'}]},
-        {background: '#b8c5f2', fonts: [{text: '3'}]},
-        {background: '#e9e8fe', fonts: [{text: '4'}]},
-        {background: '#b8c5f2', fonts: [{text: '5'}]},
-    ])
+    const [prizes, setPrizes] = useState([{}])
+    useEffect(() => {
+        queryRaffleAwardListHandle().then(r => {
+        });
+    }, [])
+
+    const queryRaffleAwardListHandle = async () => {
+        const result = await queryRaffleAwardList(strategyId);
+        const {code ,info, data} = await result.json();
+        if (code != "0000") {
+            window.alert("获取奖品列表失败 info: " + info + "  code:" + code);
+            return;
+        }
+        const prizes = data.map((award : RaffleAwardVO, index : number) => {
+            const background = index % 2 === 0 ? '#e9e8fe' : '#b8c5f2';
+            return {
+                background: background,
+                fonts: [{id: award.awardId, text: award.awardTitle, top: '15px'}]
+            };
+        })
+        setPrizes(prizes)
+    }
+
+    const randomRaffleHandle = async () => {
+        const result = await randomRaffle(strategyId);
+        const {code ,info, data} = await result.json();
+        if (code != "0000") {
+            window.alert("抽奖失败 info: " + info + "  code:" + code);
+            return;
+        }
+        return data.awardId
+    }
+
     const [buttons] = useState([
         {radius: '40%', background: '#617df2'},
         {radius: '35%', background: '#afc8ff'},
@@ -41,9 +70,12 @@ export function LuckyWheelPage()  {
                 // @ts-ignore
                 myLucky.current.play()
                 setTimeout(() => {
-                    const index = Math.random() * 6 >> 0
-                    // @ts-ignore
-                    myLucky.current.stop(index)
+                    // 抽奖接口
+                    randomRaffleHandle().then(prizeIndex => {
+                            // @ts-ignore
+                            myLucky.current.stop(prizeIndex);
+                        }
+                    );
                 }, 2500)
             }}
             onEnd={
