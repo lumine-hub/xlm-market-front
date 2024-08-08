0d40292540bf33eb21912c80139b464f9abf4186
lumine-hub <2761151386@qq.com>
2024-08-08 19:51:02 +0800

    活动信息API迭代和功能完善

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,32 +1,35 @@
 // 请求地址
-const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "http://localhost:8091";
-
+const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "https://localhost:8080";
 
 /**
- * 奖品装配接口
+ * 装配抽奖
+ * @param activityId
  */
-
-export const strategyArmory = (strategyId: number) => {
-    return fetch(`${apiHostUrl}/api/v1/raffle/strategy_armory?strategyId=${strategyId}`, {
-        method: 'get',
+export const activityStrategyArmory = (activityId?: number) => {
+    return fetch(`${apiHostUrl}/api/v1/raffle/activity/armory?activityId=${activityId}`, {
+        method: 'GET',
         headers: {
-            'Content-Type': 'application/json;charset=utf-8'
+            'Content-Type': 'application/json'
         }
-    });
+    })
 }
 
 /**
  * 查询抽奖奖品列表
- * @param strategyId 策略ID
+ * @param userId 用户ID
+ * @param activityId 用户ID
  */
-export const queryRaffleAwardList = (strategyId?: number) => {
+export const queryRaffleAwardList = (userId?: string, activityId?: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list`, {
-            method: 'post',
+        return fetch(`${apiHostUrl}/api/v1/raffle/strategy/query_raffle_award_list`, {
+            method: 'POST',
             headers: {
-                'Content-Type': 'application/json;charset=utf-8'
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify({strategyId: strategyId})
+            body: JSON.stringify({
+                userId: userId,
+                activityId: activityId
+            })
         });
     } catch (error) {
         return fetch("{\n" +
@@ -38,27 +41,21 @@ export const queryRaffleAwardList = (strategyId?: number) => {
 }
 
 /**
- * 随机抽奖接口
- * @param strategyId 策略ID
- *
- * {
- * 	"code": "0000",
- * 	"info": "调用成功",
- * 	"data": {
- * 	    "awardIndex": 1, // awardIndex 获得的是列表中第几个奖品，方便测试使用
- * 		"awardId": 535,
- * 		"awardTitle": "一部手机"
- * 	}
- * }
+ * 抽奖接口
+ * @param userId 用户ID
+ * @param activityId 用户ID
  */
-export const randomRaffle = (strategyId: number) => {
+export const draw = (userId?: string, activityId?: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle`, {
-            method: 'post',
+        return fetch(`${apiHostUrl}/api/v1/raffle/activity/draw`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
-            body: JSON.stringify({strategyId: strategyId})
+            body: JSON.stringify({
+                userId: userId,
+                activityId: activityId
+            })
         })
     } catch (error) {
         return fetch("{\n" +
